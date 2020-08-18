041702bb1e5cf7c2964fe0a9627d43ebc56a01d9
Peter Wells <petethebeatwells@gmail.com>
2020-08-18 11:35:34 -0700

    adds SizeOfTypes func to see just how many bytes these types take up

--- src/C4_Drills.cpp
+++ src/C4_Drills.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+void SizeOfTypes();
+
 void Chapter4Drills()
 {
-	std::cout << "hi";
+	SizeOfTypes();
+}
+
+void SizeOfTypes()
+{
+	std::cout << "char: " << sizeof(char) << std::endl;
+	std::cout << "int: " << sizeof(int) << std::endl;
+	std::cout << "float: " << sizeof(float) << std::endl;
+	std::cout << "double: " << sizeof(double) << std::endl;
+	std::cout << "long long int: " << sizeof(long long int) << std::endl;
 }
