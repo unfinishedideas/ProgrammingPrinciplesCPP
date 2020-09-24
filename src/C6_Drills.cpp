ddce12fca0f2334c64e1ae165e6cf307ef231274
Peter Wells <petethebeatwells@gmail.com>
2020-09-23 18:13:06 -0700

    attempting to remove bin-int folder stuffs from git

--- src/C6_Drills.cpp
+++ src/C6_Drills.cpp
@@ -26,55 +26,55 @@ void Chapter6Drills()
 
 void c6TryThis()
 {
-	std::cout << "hi";
+	std::cout << "Chapter 6 - Try this\n";
 }
 
 void c6Drill1()
 {
-
+	std::cout << "Chapter 6 - Drill 1\n";
 }
 
 void c6Drill2()
 {
-
+	std::cout << "Chapter 6 - Drill 2\n";
 }
 
 void c6Drill3()
 {
-
+	std::cout << "Chapter 6 - Drill 3\n";
 }
 
 void c6Drill4()
 {
-
+	std::cout << "Chapter 6 - Drill 4\n";
 }
 
 void c6Drill5()
 {
-
+	std::cout << "Chapter 6 - Drill 5\n";
 }
 
 void c6Drill6()
 {
-
+	std::cout << "Chapter 6 - Drill 6\n";
 }
 
 void c6Drill7()
 {
-
+	std::cout << "Chapter 6 - Drill 7\n";
 }
 
 void c6Drill8()
 {
-
+	std::cout << "Chapter 6 - Drill 8\n";
 }
 
 void c6Drill9()
 {
-
+	std::cout << "Chapter 6 - Drill 9\n";
 }
 
 void c6Drill10()
 {
-
+	std::cout << "Chapter 6 - Drill 10\n";
 }
